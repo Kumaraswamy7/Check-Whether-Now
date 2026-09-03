3225fe485ef7f801fb9599efc749e8dd6f7fce2c
A. Kumaraswamy <adepukumaraswamy22@gmail.com>
2026-09-03 18:10:40 +0530

    Refactor weather and country APIs integration

--- script.js
+++ script.js
@@ -1,180 +1,184 @@
-let countyapi="https://countriesnow.space/api/v0.1/countries";
-const whetherapi="https://api.weatherapi.com/v1/current.json?key=03913c19b8384a86afc22937241609&q=Hyderabad";
-// const flagsapi="https://restcountries.com/v3.1/name/";
-//async function flagsearch(){
-  // let flagg=await fetch(flagsapi);
-  // let flagpng=await flagg.json();
-  // console.log(flagpng[0].flags.png);
-  // let flag=document.createElement("img");
-  // flag.src=flagpng[0].flags.png;
-  // flag.alt="Flag";
-  // para.append(flag)
-//}
-//flagsearch();
-let imgflag=document.querySelector(".Flag");
-let dataof="";
-let flagpng="";
-let countryname=[];
-let cityname=[];
-let selectedCountry="India";
-let selectedCity="";
-let apiarr=[];
-const drpdowncountry=document.querySelector(".select-opt-country");
-const drpdowncity=document.querySelector(".select-opt-city");
+const countriesApi = "https://countriesnow.space/api/v0.1/countries";
+const countriesFlagApi = "https://countriesnow.space/api/v0.1/countries/flag/images";
+const weatherApiKey = "03913c19b8384a86afc22937241609";
 
-function findcity(country){
-  let count=0;
-  drpdowncity.innerHTML = "";
-  for(let k=0;k<dataof.length;k++){
-  if(dataof[k].country===country){
-    if(count===0){
-      let cityarr=dataof[k].cities;
-      for(let len=0;len<cityarr.length;len++){
-       //console.log(cityarr[len]);
-      let cityopt=document.createElement("option");
-      let one=document.createElement("option");
-      one.innerText="---";
-      cityopt.innerText=cityarr[len];
-      cityopt.value=cityarr[len];
-      drpdowncity.prepend(one);
-      drpdowncity.append(cityopt);
-       if(len==cityarr.length-1){
-  drpdowncity.addEventListener("change", function() {
-    //console.log(drpdowncity.value);
-  selectedCity = drpdowncity.value;
-  getwhether(country,selectedCity);
-});
-         break;
-       }
-      }
-    count=1;
-    }
-  }
-}
-}
-function find(countryname,i){
-  //console.log(countryname,i);
-  //console.log(dataof[i].cities)
-  //console.log(drpdowncountry)
- 
-  let countryoptn=document.createElement("option");
-   countryoptn.innerText=countryname;
-   countryoptn.value=countryname;
-   drpdowncountry.append(countryoptn);
-   drpdowncountry.value="India";
-  // let cityopt=document.createElement("option");
-  // cityopt.innerText=dataof[i].cities[i];
-  // drpdowncity.append(cityopt);
-  
-}
-function display(dataof){
- // console.log("displaying the country data");
-  let message=[];
-  for(let i=0;i<dataof.length;i++){
-    let city=dataof[i];
-    countryname.push(city.country);
-    find(countryname[i],i);
-  }
-  //console.log(countryname);
+const flagImage = document.querySelector(".Flag");
+const countrySelect = document.querySelector(".select-opt-country");
+const citySelect = document.querySelector(".select-opt-city");
+const getButton = document.querySelector(".get-whether-btn button");
+const dataBoxes = Array.from(document.querySelectorAll(".disdata"));
+
+let countries = [];
+let flagByCountry = new Map();
+
+function setSelectMessage(select, message) {
+  select.innerHTML = "";
+  const option = document.createElement("option");
+  option.value = "";
+  option.textContent = message;
+  option.disabled = true;
+  option.selected = true;
+  select.append(option);
 }
 
-const countryy=async ()=>{
-  let apiofcountry=await fetch(countyapi);
-  let countrydata=await apiofcountry.json();
-  dataof=await countrydata.data;
-  ifcon(); 
-  display(dataof);
+function setWeatherMessage(message) {
+  dataBoxes.forEach((box, index) => {
+    box.textContent = index === 0 ? message : "";
+  });
 }
-function addtopage(whetherarr){
-  apiarr=[];
-  //console.log(whetherarr);
-  for(let i=0;i<whetherarr.length;i++){
-    if(i<whetherarr.length-1){
-    apiarr[i]=document.querySelector(`.apidata${i+1}`);
-    }
-    if(i==0){
-    for(let j=0;j<1;j++){
-    apiarr[i].innerText=`${whetherarr[j]}`;
-    }
-    }
-    else if(i>0){
-    apiarr[i].innerText=`${whetherarr[i+1]}`;
-    }
-    //console.log(whetherarr);
+
+function updateWeatherBoxes(weather) {
+  const values = [
+    `${weather.current.temp_c} C`,
+    weather.current.condition.text,
+    `${weather.current.wind_kph} kph`,
+    `${weather.current.pressure_in} in`,
+    weather.current.wind_dir,
+    `${weather.current.humidity}%`,
+    `${weather.current.cloud}%`,
+    weather.current.uv,
+  ];
+
+  dataBoxes.forEach((box, index) => {
+    box.textContent = values[index] ?? "";
+  });
+}
+
+async function fetchJson(url, options) {
+  const response = await fetch(url, options);
+
+  if (!response.ok) {
+    throw new Error(`Request failed with status ${response.status}`);
   }
-     //console.log(apiarr) 
+
+  const data = await response.json();
+
+  if (data.error) {
+    throw new Error(data.error.message || data.msg || "The API returned an error.");
+  }
+
+  return data;
 }
-async function selectcity(citynaame,countrynamee){
-  const whetherarr=[];
-//   drpdowncity.addEventListener("change", function() {
-//   selectedCity = drpdowncity.value;
-//   selectcity(selectedcity);
-//   console.log(selectedcity);
-// });
-//console.log(citynaame);
-  let whetherurl=`https://api.weatherapi.com/v1/current.json?key=03913c19b8384a86afc22937241609&q=${citynaame},${countrynamee}`;
-  let citydata=await fetch(whetherurl);
-  let cityy=await citydata.json();
-  let cityof=cityy.current;
-  //console.log(cityof);
-  //console.log("real temp",cityof.temp_c);
-  whetherarr.push(cityof.temp_c);
-  //console.log("flees like",cityof.feelslike_c);
-  whetherarr.push(cityof.feelslike_c);
- // console.log("text",cityof.condition.text);
-  whetherarr.push(cityof.condition.text);
-  //console.log("wind kph",cityof.wind_kph);
-  whetherarr.push(cityof.wind_kph);
-  //console.log("pressure",cityof.pressure_in);
-  whetherarr.push(cityof.pressure_in);
- // console.log("wind_direction",cityof.wind_dir);
-  whetherarr.push(cityof.wind_dir);
- // console.log("humidity",cityof.humidity);
-    whetherarr.push(cityof.humidity);
- // console.log("cloud",cityof.cloud);
-  whetherarr.push(cityof.cloud);
- // console.log("UV",cityof.uv);
-  whetherarr.push(cityof.uv);
-  addtopage(whetherarr);
+
+function populateCountries() {
+  countrySelect.innerHTML = "";
+
+  countries.forEach(({ country }) => {
+    const option = document.createElement("option");
+    option.value = country;
+    option.textContent = country;
+    countrySelect.append(option);
+  });
+
+  const defaultCountry = countries.some(({ country }) => country === "India")
+    ? "India"
+    : countries[0]?.country;
+
+  if (defaultCountry) {
+    countrySelect.value = defaultCountry;
+    populateCities(defaultCountry);
+    updateFlag(defaultCountry);
+  }
 }
-const ifcon=async ()=>{
-  if(selectedCountry==="India"){
-     const flagsapi=`https://restcountries.com/v3.1/name/India`;
-  let flagg=await fetch(flagsapi);
-  let flagsrc=await flagg.json();
-  flagpng=flagsrc[0].flags.png;
-  imgflag.src=flagpng;
-  findcity(selectedCountry);
-   }
+
+function populateCities(countryName) {
+  const country = countries.find((item) => item.country === countryName);
+  const cities = country?.cities || [];
+
+  citySelect.innerHTML = "";
+
+  if (!cities.length) {
+    setSelectMessage(citySelect, "No cities found");
+    return;
+  }
+
+  const placeholder = document.createElement("option");
+  placeholder.value = "";
+  placeholder.textContent = "Select city";
+  placeholder.disabled = true;
+  placeholder.selected = true;
+  citySelect.append(placeholder);
+
+  cities.forEach((city) => {
+    const option = document.createElement("option");
+    option.value = city;
+    option.textContent = city;
+    citySelect.append(option);
+  });
 }
- drpdowncountry.addEventListener("change", async function() {
-   
-   selectedCountry = drpdowncountry.value;
- const flagsapi=`https://restcountries.com/v3.1/name/${drpdowncountry.options[drpdowncountry.selectedIndex].value}`;
-  let flagg=await fetch(flagsapi);
-  let flagsrc=await flagg.json();
-  flagpng=flagsrc[0].flags.png;
-  imgflag.src=flagpng;
-  drpdowncity.append("-");
-   findcity(selectedCountry);
-   
-   // Get the value of the selected option
-  //console.log("Selected country:", selectedCountry);
-});
-let getbtn=document.querySelector(".get-whether-btn button");
-getbtn.addEventListener("click",(event)=>{
+
+function updateFlag(countryName) {
+  const flagUrl = flagByCountry.get(countryName);
+  flagImage.src = flagUrl || "";
+  flagImage.alt = flagUrl ? `${countryName} flag` : "";
+}
+
+async function loadCountries() {
+  setSelectMessage(countrySelect, "Loading...");
+  setSelectMessage(citySelect, "Loading...");
+  setWeatherMessage("Choose a city");
+
+  try {
+    const [countryData, flagData] = await Promise.all([
+      fetchJson(countriesApi),
+      fetchJson(countriesFlagApi),
+    ]);
+
+    countries = countryData.data || [];
+    flagByCountry = new Map(
+      (flagData.data || []).map(({ name, flag }) => [name, flag])
+    );
+
+    populateCountries();
+  } catch (error) {
+    console.error(error);
+    setSelectMessage(countrySelect, "Countries unavailable");
+    setSelectMessage(citySelect, "Cities unavailable");
+    setWeatherMessage("Unable to load countries");
+  }
+}
+
+async function loadWeather(event) {
   event.preventDefault();
-});
-function getwhether(country,select2){
-getbtn.addEventListener("click",(event)=>{
-  event.preventDefault();
-  //let selectedcityy=drpdowncity.value;
-  drpdowncity.addEventListener("change", function() {
-  selectedCity = drpdowncity.value;
-  //selectcity(selectedcity);
-  //console.log(selectedcity);
-});
-  selectcity(select2,country);
-});
+
+  const countryName = countrySelect.value;
+  const cityName = citySelect.value;
+
+  if (!countryName || !cityName) {
+    setWeatherMessage("Select a city first");
+    return;
+  }
+
+  setWeatherMessage("Loading weather...");
+  getButton.disabled = true;
+
+  try {
+    const query = encodeURIComponent(`${cityName},${countryName}`);
+    const weather = await fetchJson(
+      `https://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=${query}`
+      // https://api.weatherapi.com/v1/current.json?key=${03913c19b8384a86afc22937241609}`
+    );
+
+    updateWeatherBoxes(weather);
+  } catch (error) {
+    console.error(error);
+    setWeatherMessage("Weather unavailable");
+  } finally {
+    getButton.disabled = false;
+  }
 }
-countryy();
+
+countrySelect.addEventListener("change", () => {
+  const countryName = countrySelect.value;
+  populateCities(countryName);
+  updateFlag(countryName);
+  setWeatherMessage("Choose a city");
+});
+
+citySelect.addEventListener("change", () => {
+  setWeatherMessage("Ready");
+});
+
+getButton.addEventListener("click", loadWeather);
+
+loadCountries();
